1d481ed2f558558d8cae98ffd0171c6e14cc0fe9
Vijay Singh <vijaysinghaashu@gmail.com>
2023-08-26 12:23:52 +0530

    first commit

---
+++ src/main/java/com/excellence/usernamelogin/FormFileApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.usernamelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/usernamelogin/FormFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.usernamelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
